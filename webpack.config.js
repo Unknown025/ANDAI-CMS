19b106ab0c6327474579d213b56313c0209968ed
Sasha Petushkov <Unknown025@users.noreply.github.com>
2019-12-13 13:24:01 -0800

    Installed MDC, webpack.

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const autoPrefixer = require('autoprefixer');
+
+module.exports = {
+    // mode: 'production',
+    entry: {
+        index: ['./site/css/style.scss', './site/js/index.js'],
+    },
+    output: {
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].css',
+                        },
+                    },
+                    {loader: 'extract-loader'},
+                    {loader: 'css-loader'},
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoPrefixer()]
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                includePaths: ['./node_modules'],
+                            }
+                        },
+                    }
+                ],
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/preset-env'],
+                },
+            }
+        ],
+    },
+};
